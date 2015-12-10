7cabc771fc5ac66881779eba5d4d2d1a542aa570
Matt <matt@Matts-Mac-Pro.local>
2015-12-10 18:42:34 +0000

    SUBMITTED VERSION

--- Battleships.ino
+++ Battleships.ino
@@ -1,11 +1,37 @@
+//
+//			ARDUINO BATTLESHIPS
+//
+//			AUTHORS:			
+//			  MATT BUCKLEY
+//			  RICCARDO BROGGI
+//
+
+
+/*
+Packet structure:
+
+	Start of Transmission (SOT)
+	Player Number (To check we're in sync with other device)
+	Game State (To check we're in sync with other device)
+
+	--The rest of the data is dependant on the gameState
+
+	End of Transmission (EOT)
+
+*/
+
+
+
+
+
 // ------------------------ Definitions -----------------------
 
 #include <SoftwareSerial.h>
 #include <stdbool.h>
 
 //Tansmission Definitions
-#define SOT 's' //Start of transmission
-#define EOT 'e' //End of transmission
+#define SOT 's' //Start of transmission char
+#define EOT 'e' //End of transmission char
 
 //Pin Definitions
 #define RXPIN 3
@@ -15,25 +41,25 @@
 #define SPEAKERPIN 12
 
 //Game definitions
-#define MAXSHIPS 3
-#define SHIPTOTAL 8
+#define MAXSHIPS 3 //Maximum number of ships a player can have
+#define SHIPTOTAL 8 //Number of LEDs
 
 //Game Variables
 int ledPin[] = {4, 5, 6, 7, 8, 9, 10, 11};
 int speedInitial[] = {150, 100, 50, 30, 20}; //For light display
 int gameState = 0; //0 is menu, 1 is choosing ships, 2 is attacking ships, 3 is game over
 volatile bool buttonPressed;
-int activePlayer;
-int myPlayer = -1;
-int otherPlayer = -1;
-bool playerReady[2];
-bool playerWon[2];
-int shipLocation[2][MAXSHIPS];
-bool shipTaken[SHIPTOTAL];
+int activePlayer; //Which player's turn is it?
+int myPlayer = -1; //Default is -1 for undecided
+int otherPlayer = -1; //Default is -1 for undecided
+bool playerReady[2]; //Ready to continue to next game state
+bool playerWon[2]; //If a player wins, they send playerWon[Their Player Number] = true
+int shipLocation[2][MAXSHIPS]; //LED locations of each player's ships
+bool shipTaken[SHIPTOTAL]; //Stores if an LED already has a ship assigned to it
+bool shipDestroyed[2][MAXSHIPS]; //Keeps track of whether each player's ships are destroyed
 bool errorMode = true;
 bool warningMode = true; 
 bool debugMode = true;
-bool shipDestroyed[2][MAXSHIPS];
 
 //SoftwareSerial
 SoftwareSerial serial(RXPIN, TXPIN);
@@ -44,17 +70,17 @@ int winTones[] = {523, 587, 659, 698, 784, 880, 1046, 1174};
 // ------------------------ Setup and Loop -----------------------
 
 void setup() {
-comSetup();
-gameSetup();
-
+	comSetup();
+	gameSetup();
 }
 
 //Main Loop
  void loop() {
 	clearLEDs();
-	comReceive();
-	gameLoop();
+	comReceive(); //Receive Communication
+	gameLoop(); //Main game loop, contains game logic and sends data
 }
+
 
 // ------------------------ Function definitions -----------------------
 
@@ -64,7 +90,7 @@ void buttonInterrupt() {
 }
 
 
-//Setup Communication
+//Setup Communication. Connections are made and player numbers decided here
 void comSetup() {
 	//Setup SoftwareSerial
 	pinMode(RXPIN, INPUT);
@@ -74,61 +100,60 @@ void comSetup() {
 	//Setup Debug Serial
 	if (debugMode || errorMode || warningMode) Serial.begin(57600);
 
+	//Flush serial buffer in case other device has been on for a while, our buffer may be full if our reset button has been pushed
+	flushSerial();
+
+
 	//Wait for connection
+	//This involved 3 steps
+	//Step 1: send 'h'
+	//Step 2: has received h, so send 'i'
+	//Step 3: if you receive an 'i', a connection has been made. Send 'i' to tell the other device
 	deb("Waiting for connection");
 	bool step1 = false;
 	bool step2 = false;
 	char data;
 
-	flushSerial(); //In case other device has been on for a while
-
 	while (!step2) {
-		//delay(500);
-
-		if (serial.available() > 0) {
-			data = serial.read();
+		if (serial.available() > 0) { //If we have received anything
+			data = serial.read(); //Read received data
 			switch (data) {
-				case 'h':
+				case 'h': //If we received h
 					step1 = true;
-					//deb("Received h");
 				break;
-				case 'i':
+				case 'i': //If we received i
 					step1 = true;
 					step2 = true;
-					//deb("Received i");
-					serial.print('i');
+					serial.print('i'); //Send i
 				break;
 			}
 		}
-
-		if (!step1) {
-			serial.print('h');
-			//deb("Pushing h");
+		if (!step1) { //If we haven't received h yet
+			serial.print('h'); //Send h
 		} else {
-			serial.print('i');
-			//deb("Pushing i");
+			serial.print('i'); //Else, send i
 		}
 	}
 
+	//Connection made. Flush serial buffer in case any 'h' or 'i' are left in the buffer
 	deb("Connection made");
 	flushSerial();
 
+
 	//Decide player numbers
 	deb("Deciding player numbers");
-	while(otherPlayer == -1) {
-		//delay(500);
-		if (serial.available() > 0) {
-			data = serial.read();
+	while(otherPlayer == -1) { //While we don't know the other player's number
+		if (serial.available() > 0) { //If we received data
+			data = serial.read(); //Read data. This data is the other player's number
 			switch (data) {
-				case '0': {
+				case '0': { //Received other player is 0. We are 1
 					otherPlayer = 0;
 					myPlayer = 1;
 					deb("I am player 1");
 					serial.print('1');
 					break;
 				}
-
-				case '1': {
+				case '1': { //Received other player is 1. They accepted us as player 0
 					otherPlayer = 1;
 					myPlayer = 0;
 					deb("I am player 0");
@@ -137,13 +162,16 @@ void comSetup() {
 				}
 			}
 		} else {
-			serial.print('0');
+			//If we haven't received anything, tell other player we are 0 and see if we get a 1 back from them
+			serial.print('0'); //Send 0
 		}
 	}
 
+	//Numbers assigned. Flush serial buffer in case any '0' or '1' are left in the buffer
 	deb("Player numbers have been assigned.");
 	flushSerial();
 }
+
 
 //Receive communication
 void comReceive() {
@@ -169,6 +197,7 @@ void comReceive() {
 			//The rest of the packet structure depends on the current gameState
 			switch (gameState) {
 				case 0: { //Menu
+					//Receive whether or not they are ready to move to next gameState (if they pushed their button)
 					playerReady[pNum] = serial.read();
 					deb("Received pReady", playerReady[pNum]);
 					if (playerReady[otherPlayer]) deb("Other player is ready.");
@@ -176,10 +205,12 @@ void comReceive() {
 				}
 
 				case 1: { //Selecting player's own ships
+					//Recived other player's ship locations
 					for (int i=0; i<MAXSHIPS; i++) {
 						shipLocation[pNum][i] = serial.read();
 						deb("Received Ship Location", shipLocation[pNum][i]);
 					}
+					//Receive whether or not they are ready to move to next gameState (If they selected MAXSHIPS ships)
 					playerReady[pNum] = serial.read();
 					deb("Received pReady", playerReady[pNum]);
 					if (playerReady[otherPlayer]) deb("Other player has selected ships and is ready.");
@@ -187,22 +218,23 @@ void comReceive() {
 				}
 
 				case 2: { //Attacking the other player's ships
-					int shipTheyDestroyed = serial.read();
-					if (shipTheyDestroyed != 255) {
+					//Received the destroyed ship
+					//225 means that they missed their shot
+					int shipTheyDestroyed = serial.read(); //Read data
+					if (shipTheyDestroyed != 255) { //If they actually hit a ship
 						shipDestroyed[myPlayer][shipTheyDestroyed] = true;
+						//Play explosion sound
 						exploded(shipLocation[myPlayer][shipTheyDestroyed]);
 						deb("They destroyed our ship", shipTheyDestroyed);
 					} else {
 						deb("They missed their shot", shipTheyDestroyed);
 					}
+					//Receive which player's turn it is
 					activePlayer = serial.read();
 					deb("Received active player: ", activePlayer);
+					//Receive whether or not the other player has won
 					playerWon[otherPlayer] = serial.read();
 					deb("Received other player won: ", playerWon[otherPlayer]);
-					break;
-				}
-
-				case 3: { //Game over
 					break;
 				}
 			}
@@ -213,6 +245,7 @@ void comReceive() {
 				deb("Received EOT");
 			}
 		} else {
+			//There were incomplete packets left in the buffer from a previous gameState (usually setup)
 			warn("Received packet that doesn't start with SOT, flushing serial buffer");
 			flushSerial();
 		}	
@@ -222,33 +255,37 @@ void comReceive() {
 //Game Setup
 void gameSetup() {
 	deb("Setting up game");
+	//Set up button
 	attachInterrupt(digitalPinToInterrupt(BUTTONPIN), buttonInterrupt, FALLING);
+	//Set LED pinModes
 	for (int i=0; i<countLEDs(); i++) {
 		pinMode(ledPin[i], OUTPUT);
 	}
+	//Set all ship locations to default value -1 to indicate an unset location
 	for (int i=0; i<MAXSHIPS; i++) {
 		shipLocation[myPlayer][i] = -1;
 	}
+	//Set speaker pinMode
 	pinMode(SPEAKERPIN, OUTPUT);
 }
 
-//Main Game Loop
+
+//Main Game Loop, all communication is sent from here
 void gameLoop() {
-	int ship = readPotentiometer();
+	int ship = readPotentiometer(); //Get led LED that the potentiometer is currently pointing towards
 	switch (gameState) {
 		case 0: { //Menu
-			//Play music until both players push their button
-			//Do some fancy shit with LEDs
 			//Check for button presses
 			if (buttonPressed) {
 				if (!playerReady[myPlayer]) {
 					deb("My player is ready.");
 					playerReady[myPlayer] = true;
-					serial.print(SOT);
-					serial.write(myPlayer);
-					serial.write(gameState);
-					serial.write(playerReady[myPlayer]);
-					serial.print(EOT);
+					//Tell the other player we're ready to continue
+					serial.print(SOT); //Send start of transmission char
+					serial.write(myPlayer); //Send our player number to check we're in sync
+					serial.write(gameState); //Send our gameState to check we're in sync
+					serial.write(playerReady[myPlayer]); //Send whether or not we are ready (we are)
+					serial.print(EOT); //Send end of transmission character
 				}
 				buttonPressed = false;
 			}
@@ -257,6 +294,7 @@ void gameLoop() {
 				//Move on to next game state
 				gameState = 1;
 				deb("Both players are ready. Moving to gameState 1");
+				//Reset pReadys and flush serial buffer for anything left over from incomplete packets
 				playerReady[0] = false;
 				playerReady[1] = false;
 				flushSerial();
@@ -303,6 +341,7 @@ void gameLoop() {
 			}
 
 			if (playerReady[myPlayer]) {
+				//Light up the LEDs that represent our ships
 				displayMyShips();
 			}
 			break;
@@ -386,7 +425,6 @@ void gameLoop() {
 // the potentiometer can take into 7 "regions"; the potentiometer can take values from 0 to
 // 1023 so every 128 corresponds to a a value, meaning it will light up the corresponding LED
 // when the value returned by this function is used.
-
 int readPotentiometer() {
 	int valueRead = analogRead(POTPIN);
 	if(valueRead <= 128){
@@ -426,20 +464,20 @@ void flushSerial() {
 	}
 }
 
-// Define a function that lights off every LED;
+// Define a function that turns off every LED;
 void clearLEDs() {
 	for (int i=0; i<countLEDs(); i++) {
 		digitalWrite(ledPin[i], LOW);
 	}
 }
 
-// Define a function that returns the number of LEDs
+// Define a function that returns the number of LED pins in the LedPin array
 int countLEDs() {
 	return(sizeof(ledPin) / sizeof(ledPin[0]));
 }
 
-// Define a function that returns the next free ship by looping through the locations and checking
-// which are emoty and which are not
+// Define a function that returns the next shipLocation which is set to -1. Because -1 is the default value, this is used to set the next ship when selecting ships at the start in gameState=1
+// If all ships already have positions allocated, it returns -1
 int nextFreeShip() {
 	for (int i=0; i<MAXSHIPS; i++) {
 		if (shipLocation[myPlayer][i] == -1) {
@@ -463,7 +501,7 @@ int enemyShipAtLocation(int location) {
 }
 
 // Define a funciton that returns a boolean corresponding to whether the active player has won
-// or not by looping through all of the ships, checking how many have been destroed and checking 
+// or not by looping through all of the ships, checking how many have been destroyed and checking 
 // whether the number of ships destroyed corresponds to the number of total ships. If so, return true
 // else return false
 bool haveWeWon() {
@@ -480,6 +518,7 @@ bool haveWeWon() {
 	}
 }
 
+//Lights up all the LEDs which represent your player's ships
 void displayMyShips() {
 	for (int i=0; i<MAXSHIPS; i++) {
 		if (!shipDestroyed[myPlayer][i]) {
@@ -488,6 +527,7 @@ void displayMyShips() {
 	}
 }
 
+//Lights up the LEDs in a circle while playing the win sound when a player has won
 void lightDisplay(){
 	for(int i=0; i<8; i++) {
 		int initialGame = speedInitial[i];
@@ -502,6 +542,8 @@ void lightDisplay(){
 	noTone(SPEAKERPIN);
 }
 
+
+
 //   -----------------Define Sound Functions-----------------------------
 
 // Define a function to play a sound that will be implemented by the missleFired and exploded functions.
@@ -515,6 +557,7 @@ void playSound(long frequency, long lengthSound){
     delayMicroseconds(period/2);
   }
 }
+
 // Define a function that uses the playSound one to play the sound of a missle that has been fired
 void missleFired(){
 	for(int i = 99; i < 1002; i += 5){
@@ -542,13 +585,14 @@ void succeedSound(){
 	tone(SPEAKERPIN, 658);
 	delay(300);
 	noTone(SPEAKERPIN);
-	delay(500); }
+	delay(500);
+}
+
+
 
 //   -----------------Define Debugging Functions-----------------------------
 
-
-
-//Prints out an error message to the debug
+//Prints out an error message to the debug serial monitor if errorMode is set at the top of the file
 void err(char* message) {
 	if (errorMode) {
 		Serial.print("ERROR: ");
@@ -556,6 +600,7 @@ void err(char* message) {
 	}
 }
 
+//Prints out a warning message to the debug serial monitor if warningMode is set at the top of the file
 void warn(char* message) {
 	if (warningMode) {
 		Serial.print("WARNING: ");
@@ -563,6 +608,7 @@ void warn(char* message) {
 	}
 }
 
+//Prints out a debug message to the debug serial monitor if debugMode is set at the top of the file
 void deb(char* message) {
 	if (debugMode) {
 		Serial.print("DEBUG: ");
@@ -570,6 +616,7 @@ void deb(char* message) {
 	}
 }
 
+//Prints out a debug message following by a specified integer to the debug serial monitor if debugMode is set at the top of the file
 void deb(char* message, int integer) {
 	if (debugMode) {
 		Serial.print("DEBUG: ");
